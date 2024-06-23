812e56ab38a5504fab84c8e826c7d71f9675e296
Aryant Soni <aryantshrirang4@gmail.com>
2024-06-23 17:33:05 +0530

    button hover problem solved in mobile 3

--- src/Components/Btn.jsx
+++ src/Components/Btn.jsx
@@ -3,31 +3,31 @@ import "../style/Btn.css"
 import PropTypes from 'prop-types';
 
 const Btn = ({ t1, tm, t2, t3, t4, t5 }) => {
- 
+    const [py, setpy] = useState(false);
     return (
         <>
-            <button className={`button rounded-full  phone:hover:my-14 `}   >
+            <button className={`button rounded-full ${py ? "phone:hover:my-14" : " phone:hover:my-0"} `}  onClick={()=> setpy(!py)} >
                 <div className="icon">
-                    <span className="text-icon hide">{t1}</span>
+                    <span className={`text-icon ${py?"":"phone:hide "} hide  `}>{t1}</span>
                     <svg className="css-i6dzq1" strokeLinejoin="round" strokeLinecap="round" fill="none" strokeWidth={2} stroke="currentColor" height={24} width={24} viewBox="0 0 24 24">
                         <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z" />
                     </svg>
                 </div>
                 <span className="title text-xl px-3"> {tm} </span>
-                <div className="padding-left hide">
+                <div className={`padding-left ${py?"":"phone:hide "} hide  `}>
                     <div className="padding-left-line">
                         <span className="padding-left-text">{t2}</span>
                     </div>
                 </div>
-                <div className="padding-right hide">
+                <div className={`padding-right ${py?"":"phone:hide "} hide  `}>
                     <div className="padding-right-line">
                         <span className="padding-right-text">{t5}</span>
                     </div>
                 </div>
-                <div className="background hide">
+                <div className={`background ${py?"":"phone:hide "} hide  `}>
                     <span className="background-text">{t3}</span>
                 </div>
-                <div className="border hide">
+                <div className={`border ${py?"":"phone:hide "} hide  `}>
                     <span className="border-text">{t4}</span>
                 </div>
             </button>
